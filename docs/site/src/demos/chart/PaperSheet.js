ae77b8e8f3148483a16abfeaabbaaf338b7abf0a
Steve Hall <hall.steven.g@gmail.com>
2017-01-20 21:40:34 -0800

    udpate test chart demo

---
+++ docs/site/src/demos/chart/PaperSheet.js
@@ -0,0 +1,34 @@
+// @flow weak
+
+import React from 'react';
+import { createStyleSheet } from 'jss-theme-reactor';
+import customPropTypes from 'material-ui/utils/customPropTypes';
+import Paper from 'material-ui/Paper';
+import Text from 'material-ui/Text';
+
+const styleSheet = createStyleSheet('PaperSheet', (theme) => ({
+  root: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+  }),
+}));
+
+export default function PaperSheet(props, context) {
+  const classes = context.styleManager.render(styleSheet);
+  return (
+    <div>
+      <Paper className={classes.root} zDepth={4}>
+        <Text type="headline" component="h3">
+          This is a sheet of paper.
+        </Text>
+        <Text type="body1" component="p">
+          Paper can be used to build surface or other elements for your application.
+        </Text>
+      </Paper>
+    </div>
+  );
+}
+
+PaperSheet.contextTypes = {
+  styleManager: customPropTypes.muiRequired,
+};
