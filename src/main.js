20a6411bc5cd4ef83a944691a1bc0b826e0dbfd7
Steve Hall <hall.steven.g@gmail.com>
2017-02-05 19:35:26 -0800

    update main

--- src/main.js
+++ src/main.js
@@ -4,29 +4,21 @@
 const log = process.stderr;
 const str = JSON.stringify;
 
-function toNegaBinary(number) {
-  const negabinary = [];
-  const base = -2;
+function solution(input) {
 
-  let remain;
+  const merge = (a, b) => {
 
-  while (number !== 0) {
-    remain = number % base;
-    number = Math.ceil(number / base);
-    negabinary.push(remain >= 0 ? remain : -remain);
-  }
+  };
 
-  return negabinary.reverse();
+  const split = (arr) => {
+    const mid = Math.floor(arr.length / 2);
+
+  };
+
+  return split(input);
 }
 
-function solution(arr) {
-  const bits = arr.join('');
-  const deci = parseInt(bits, 2);
-
-  return (-deci).toString(2);
-}
-
-log.write(`******** ${new Date().toString()} *********** \n`);
+log.write(`********** ${new Date().toString()} *********** \n`);
 const time = process.hrtime();
 const res = solution([1, 1, 1, 1]);
 const diff = process.hrtime(time);
