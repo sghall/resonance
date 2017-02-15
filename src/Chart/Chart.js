0c70b57565cec8244301bf1ccbf80d107ed90031
Steve Hall <hall.steven.g@gmail.com>
2017-02-15 12:44:11 -0800

    clean up chart component/remove old classes

--- src/Chart/Chart.js
+++ src/Chart/Chart.js
@@ -5,33 +5,17 @@ import classNames from 'classnames';
 import { createStyleSheet } from 'jss-theme-reactor';
 import customPropTypes from '../utils/customPropTypes';
 
-export const styleSheet = createStyleSheet('Chart', (theme) => {
-  const { palette } = theme;
-  console.log(theme);
-
+export const styleSheet = createStyleSheet('Chart', () => {
   return {
     row: {
-      height: '100%',
       display: 'flex',
       flexDirection: 'row',
     },
-    wrapper: {
-      margin: '8px',
+    column: {
       border: '1px solid grey',
       display: 'flex',
-      flex: '1',
+      flex: 1,
       flexDirection: 'column',
-    },
-    chart: {
-      width: '100%',
-      height: '0px',
-      position: 'relative',
-    },
-    svg: {
-      top: 0,
-      left: 0,
-      position: 'absolute',
-      backgroundColor: palette.background.paper,
     },
   };
 });
@@ -46,40 +30,23 @@ export const styleSheet = createStyleSheet('Chart', (theme) => {
  * ```
  */
 export default function Chart(props, context) {
-  // const { className, view, trbl, children } = props;
+  const { className, view, trbl, children } = props;
   const classes = context.styleManager.render(styleSheet);
 
   return (
     <div className={classes.row}>
-      <div className={classes.wrapper}>
-        <svg viewBox="0 0 50 50">
-          <circle r="25" cx="25" cy="25" />
-        </svg>
-      </div>
-      <div className={classes.wrapper}>
-        <svg viewBox="0 0 50 50">
-          <circle r="25" cx="25" cy="25" />
+      <div className={classes.column}>
+        <svg
+          className={classNames(classes.svg, className)}
+          viewBox={`0 0 ${view[0]} ${view[1]}`}
+        >
+          <g transform={`translate(${trbl[3]} ,${trbl[0]})`}>
+            {children}
+          </g>
         </svg>
       </div>
     </div>
   );
-
-
-  // return (
-  //   <div
-  //     className={classNames(classes.chart, className)}
-  //     style={{ paddingTop: `${(Math.round(view[1] / view[0]) * 100)}%` }}
-  //   >
-  //     <svg
-  //       className={classes.svg}
-  //       viewBox={`0 0 ${view[0]} ${view[1]}`}
-  //     >
-  //       <g transform={`translate(${trbl[3]} ,${trbl[0]})`}>
-  //         {children}
-  //       </g>
-  //     </svg>
-  //   </div>
-  // );
 }
 
 Chart.propTypes = {
