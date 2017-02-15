c7ab4636806223f328bb6e15eb4cdbd79fd84b63
Steve Hall <hall.steven.g@gmail.com>
2017-02-15 15:33:09 -0800

    remove existing integration tests

--- test/utils/createShallowWithContext.js
+++ test/utils/createShallowWithContext.js
@@ -1,6 +1,6 @@
 // @flow weak
 
-import { createMuiTheme } from 'src/styles/theme';
+import { createMuiTheme } from 'material-ui/styles/theme';
 import { create } from 'jss';
 import jssPreset from 'jss-preset-default';
 import { createStyleManager } from 'jss-theme-reactor';
