a641c925e4e304f4e8708d98f16100d9a4e57eb9
Steve Hall <hall.steven.g@gmail.com>
2017-02-16 12:56:04 -0800

    update chart demo

--- docs/site/src/demos/chart/DemoChart.js
+++ docs/site/src/demos/chart/DemoChart.js
@@ -1,25 +1,31 @@
 // @flow weak
 import React from 'react';
-import customPropTypes from 'material-ui/utils/customPropTypes';
 import Chart from 'material-charts/Chart';
-import Text from 'material-ui/Text';
 
 export default function DemoChart() {
-  const view = [1000, 500];
-  const trbl = [10, 10, 10, 10];
-
   return (
-    <Chart {...{ view, trbl }}>
-      <Text type="body1" component="h3">
-        This is a sheet of paper.
-      </Text>
-      <Text type="body1" component="p">
-        Paper can be used to build surface or other elements for your application.
-      </Text>
+    <Chart
+      view={[1000, 500]}
+      trbl={[100, 100, 100, 100]}
+      style={{ backgroundColor: 'rgba(0,0,0,0.1)', marginTop: 30 }}
+    >
+      <rect
+        fill="tomato"
+        width={200}
+        height={300}
+      />
+      <rect
+        fill="tomato"
+        width={200}
+        height={300}
+        transform="translate(300, 0)"
+      />
+      <rect
+        fill="tomato"
+        width={200}
+        height={300}
+        transform="translate(600, 0)"
+      />
     </Chart>
   );
 }
-
-DemoChart.contextTypes = {
-  styleManager: customPropTypes.muiRequired,
-};
