c4a1d3c7dbfb1e9d2c70be19b61765a385907c36
Steve Hall <hall.steven.g@gmail.com>
2017-02-15 16:01:21 -0800

    update specs for Chart and Text

--- src/Chart/Chart.spec.js
+++ src/Chart/Chart.spec.js
@@ -21,24 +21,4 @@ describe('<Chart />', () => {
     );
     assert.strictEqual(wrapper.is('div'), true, 'should be a div');
   });
-
-  it('should render with the paper class, default depth class, and rounded', () => {
-    const wrapper = shallow(<Chart>Hello World</Chart>);
-    assert.strictEqual(wrapper.hasClass(classes.chart), true, 'should have the chart class');
-    assert.strictEqual(wrapper.hasClass(classes.rounded), true, 'should be rounded by default');
-  });
-
-  it('should disable the rounded class', () => {
-    const wrapper = shallow(<Chart rounded={false}>Hello World</Chart>);
-    assert.strictEqual(wrapper.hasClass(classes.rounded), false, 'should not be rounded');
-  });
-
-  it('should set the zDepth shadow class', () => {
-    const wrapper = shallow(<Chart zDepth={16}>Hello World</Chart>);
-    assert.strictEqual(wrapper.hasClass(classes.dp16), true, 'should have the dp16 shadow class');
-    wrapper.setProps({ zDepth: 24 });
-    assert.strictEqual(wrapper.hasClass(classes.dp24), true, 'should have the dp24 shadow class');
-    wrapper.setProps({ zDepth: 2 });
-    assert.strictEqual(wrapper.hasClass(classes.dp2), true, 'should have the dp2 shadow class');
-  });
 });
