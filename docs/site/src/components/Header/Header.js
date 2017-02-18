665e52822f16ed6ba5bddf4004db483c1fcfe7b0
Steve Hall <hall.steven.g@gmail.com>
2017-02-17 22:21:09 -0800

    add Header component

---
+++ docs/site/src/components/Header/Header.js
@@ -0,0 +1,19 @@
+// @flow weak
+
+import React from 'react';
+import { IndexLink, Link } from 'react-router';
+
+export const Header = () => (
+  <div>
+    <h1>React Redux Starter Kit</h1>
+    <IndexLink to="/" activeClassName="route--active">
+      Home
+    </IndexLink>
+    {' · '}
+    <Link to="/counter" activeClassName="route--active">
+      Counter
+    </Link>
+  </div>
+);
+
+export default Header;
