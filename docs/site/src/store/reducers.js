22432ab4105c218b1263eea933fc4f1d0c2bfaeb
Steve Hall <hall.steven.g@gmail.com>
2017-02-21 19:44:55 -0800

    new theme reducer/add to default set of reducers

--- docs/site/src/store/reducers.js
+++ docs/site/src/store/reducers.js
@@ -2,9 +2,11 @@
 
 import { combineReducers } from 'redux';
 import locationReducer from './location';
+import themeReducer from './theme';
 
 export const makeRootReducer = (asyncReducers) => {
   return combineReducers({
+    theme: themeReducer,
     location: locationReducer,
     ...asyncReducers,
   });
---
+++ docs/site/src/store/theme.js
@@ -0,0 +1,11 @@
+// @flow weak
+
+export default function (state = { dark: true }, action) {
+  if (action.type === 'TOGGLE_THEME_SHADE') {
+    return {
+      ...state,
+      dark: !state.dark,
+    };
+  }
+  return state;
+}
