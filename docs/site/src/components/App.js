e843433f3f0477873b2e8bfff6f86484f1b3bffe
Steve Hall <hall.steven.g@gmail.com>
2017-02-22 03:40:19 -0800

    change colors

--- docs/site/src/components/App.js
+++ docs/site/src/components/App.js
@@ -13,8 +13,40 @@ function App(props) {
   const { dark } = props;
 
   const palette = createPalette({
-    primary: brown,
-    accent: pink,
+    primary: {
+      50: '#e4e5ea',
+      100: '#bcbfcb',
+      200: '#9094a8',
+      300: '#646985',
+      400: '#42486b',
+      500: '#212851',
+      600: '#1d244a',
+      700: '#181e40',
+      800: '#141837',
+      900: '#0b0f27',
+      A100: '#6574ff',
+      A200: '#3246ff',
+      A400: '#0018fe',
+      A700: '#0016e5',
+      contrastDefaultColor: 'light',
+    },
+    accent: {
+      50: '#e5f4f3',
+      100: '#bee4e1',
+      200: '#93d3cd',
+      300: '#67c1b8',
+      400: '#47b3a9',
+      500: '#26a69a',
+      600: '#229e92',
+      700: '#1c9588',
+      800: '#178b7e',
+      900: '#0d7b6c',
+      A100: '#adfff3',
+      A200: '#7affec',
+      A400: '#47ffe4',
+      A700: '#2dffe0',
+      constrastDefaultColor: 'light',
+    },
     type: dark ? 'dark' : 'light',
   });
 
