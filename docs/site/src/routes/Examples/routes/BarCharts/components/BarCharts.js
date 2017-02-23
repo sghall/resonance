a34881003e00eeecea22d4529ec2d16aefa6a2dd
Steve Hall <hall.steven.g@gmail.com>
2017-02-22 21:20:48 -0800

    stub in content for bar chart example

--- docs/site/src/routes/Examples/routes/BarCharts/components/BarCharts.js
+++ docs/site/src/routes/Examples/routes/BarCharts/components/BarCharts.js
@@ -1,39 +1,11 @@
 // @flow weak
 
-import React from 'react';
-import { createStyleSheet } from 'jss-theme-reactor';
-import customPropTypes from 'material-ui/utils/customPropTypes';
-import Paper from 'material-ui/Paper';
-import Layout from 'material-ui/Layout';
+import { PropTypes } from 'react';
 
-const styleSheet = createStyleSheet('Examples', (theme) => ({
-  root: {
-    flexGrow: 1,
-    height: 400,
-  },
-  paper: {
-    padding: 16,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
-
-export default function Examples(props, context) {
-  const classes = context.styleManager.render(styleSheet);
-
-  return (
-    <div className={classes.root}>
-      <Layout container gutter={24}>
-        <Layout item xs={12}>
-          <Paper className={classes.paper}>
-            xs=12
-          </Paper>
-        </Layout>
-      </Layout>
-    </div>
-  );
+export default function Examples({ children }) {
+  return children;
 }
 
-Examples.contextTypes = {
-  styleManager: customPropTypes.muiRequired,
+Examples.propTypes = {
+  children: PropTypes.node,
 };
--- docs/site/src/routes/Examples/routes/BarCharts/routes/StatesByAge/components/Examples.js
+++ docs/site/src/routes/Examples/routes/BarCharts/routes/StatesByAge/components/Examples.js
@@ -1,11 +1,68 @@
 // @flow weak
 
-import { PropTypes } from 'react';
+import React from 'react';
+import { createStyleSheet } from 'jss-theme-reactor';
+import customPropTypes from 'material-ui/utils/customPropTypes';
+import Paper from 'material-ui/Paper';
+import Layout from 'material-ui/Layout';
 
-export default function Examples({ children }) {
-  return children;
+const styleSheet = createStyleSheet('CenteredLayout', (theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: 16,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function CenteredLayout(props, context) {
+  const classes = context.styleManager.render(styleSheet);
+
+  return (
+    <div className={classes.root}>
+      <Layout container gutter={24}>
+        <Layout item xs={12}>
+          <Paper className={classes.paper}>
+            xs=12
+          </Paper>
+        </Layout>
+        <Layout item xs={6}>
+          <Paper className={classes.paper}>
+            xs=6
+          </Paper>
+        </Layout>
+        <Layout item xs={6}>
+          <Paper className={classes.paper}>
+            xs=6
+          </Paper>
+        </Layout>
+        <Layout item xs={3}>
+          <Paper className={classes.paper}>
+            xs=3
+          </Paper>
+        </Layout>
+        <Layout item xs={3}>
+          <Paper className={classes.paper}>
+            xs=3
+          </Paper>
+        </Layout>
+        <Layout item xs={3}>
+          <Paper className={classes.paper}>
+            xs=3
+          </Paper>
+        </Layout>
+        <Layout item xs={3}>
+          <Paper className={classes.paper}>
+            xs=3
+          </Paper>
+        </Layout>
+      </Layout>
+    </div>
+  );
 }
 
-Examples.propTypes = {
-  children: PropTypes.node,
+CenteredLayout.contextTypes = {
+  styleManager: customPropTypes.muiRequired,
 };
