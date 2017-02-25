378023721ede085ecc6a4997ab57a1959f590f8b
Steve Hall <hall.steven.g@gmail.com>
2017-02-25 03:06:43 -0800

    keep both array and map of nodes in withSelection

--- docs/site/src/routes/examples/barCharts/statesByAge/example/components/withSelection.js
+++ docs/site/src/routes/examples/barCharts/statesByAge/example/components/withSelection.js
@@ -7,13 +7,34 @@ export const UPDATE = 'UPDATE';
 export const REMOVE = 'REMOVE';
 export const REVIVE = 'REVIVE';
 
-export function withSelection(WrappedComponent, getStringKey) {
-  class SelectionComponent extends Component {
+const keyAccessor = (d, i) => {
+  if (typeof d === 'number' || typeof d === 'string') {
+    return d;
+  }
+
+  if (d.udid) {
+    return d.udid;
+  }
+
+  return i;
+};
+
+const composeNode = (data, type, udid) => {
+  if (typeof data === 'number' || typeof data === 'string') {
+    return { data, type, udid };
+  }
+
+  return { ...data, type, udid };
+};
+
+export function withSelection(SelectionItem) {
+  class Selection extends Component {
     constructor(props) {
       super(props);
 
       this.state = {
-        nodes: {},
+        nodes: [],
+        udids: {},
       };
     }
 
@@ -23,61 +44,60 @@ export function withSelection(WrappedComponent, getStringKey) {
       }
     }
 
-    updateNodes({ data, typeKey, udidKey }) {
-      const nodes = {};
+    updateNodes(data) {
+      const nodes = [];
+      const udids = {};
 
       for (let i = 0, len = data.length; i < len; i++) {
-        const udid = getStringKey(data[i]);
+        const udid = keyAccessor(data[i], i, data);
 
-        nodes[udid] = { ...data[i], [udidKey]: udid };
+        let type = APPEAR;
 
-        if (this.state.nodes[udid]) {
-          if (this.state.nodes[udid] === REMOVE) {
-            nodes[udid][typeKey] = REVIVE;
+        if (this.state.udids[udid]) {
+          if (this.state.udids[udid] === REMOVE) {
+            type = REVIVE;
           } else {
-            nodes[udid][typeKey] = UPDATE;
+            type = UPDATE;
           }
-        } else {
-          nodes[udid][typeKey] = APPEAR;
+        }
+
+        nodes.push(composeNode(data[i], type, udid));
+        udids[udid] = type;
+      }
+
+
+      for (let i = 0, len = this.state.nodes; i < len; i++) {
+        const node = this.state.nodes[i];
+
+        if (udids[node.udid]) {
+          nodes.push(composeNode(node, REMOVE, node.udid));
         }
       }
 
-      const keys = Object.keys(this.state.nodes);
-
-      for (let i = 0, len = keys.length; i < len; i++) {
-        const key = keys[i];
-
-        if (!nodes[key] && !this.exited[key]) {
-          const node = this.state.nodes[key];
-          nodes[key] = { ...node, [typeKey]: REMOVE };
-        }
-      }
-
-      this.exited = {};
-
-      this.setState({ nodes });
+      this.setState({ nodes, udids });
     }
-
 
     render() {
       return (
-        <WrappedComponent
-          nodes={this.state.nodes} {...this.props}
-        />
+        <g>
+          {this.state.nodes.map((node) => {
+            return <SelectionItem node={node} {...this.props} />;
+          })}
+        </g>
       );
     }
   }
 
-  SelectionComponent.propTypes = {
+  Selection.propTypes = {
     data: PropTypes.array,
-    typeKey: PropTypes.string,
-    udidKey: PropTypes.string,
+    keyAccessor: PropTypes.func,
+    composeNode: PropTypes.func,
   };
 
-  SelectionComponent.defaultProps = {
-    typeKey: 'type',
-    udidKey: 'udid',
+  Selection.defaultProps = {
+    keyAccessor,
+    composeNode,
   };
 
-  return SelectionComponent;
+  return Selection;
 }
