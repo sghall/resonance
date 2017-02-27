77904285083a7064c6d1b974b99c78e1fbe89610
Steve Hall <hall.steven.g@gmail.com>
2017-02-27 15:50:20 -0800

    add temp bar node class

---
+++ docs/site/src/routes/examples/barCharts/statesByAge/example/components/BarNode.js
@@ -0,0 +1,153 @@
+// @flow weak
+
+import React, { Component, PropTypes } from 'react';
+import { timer } from 'd3-timer';
+import {
+  interpolateNumber,
+  interpolateObject,
+  interpolateTransformSvg,
+} from 'd3-interpolate';
+import { format } from 'd3-format';
+import { APPEAR, UPDATE, REMOVE, REVIVE } from './withSelection';
+
+const percentFormat = format('.2%');
+
+export default class Bar extends Component {
+
+  componentDidMount() {
+    this.onAppear(this.props);
+  }
+
+  componentWillReceiveProps(next) {
+    const { props } = this;
+
+    if (props.node !== next.node) {
+      this.transition.stop();
+
+      switch (next.node.type) {
+        case APPEAR:
+          this.onAppear(next);
+          break;
+        case UPDATE:
+          this.onUpdate(props, next);
+          break;
+        case REMOVE:
+          this.onRemove(props);
+          break;
+        case REVIVE:
+          this.onUpdate(props, next);
+          break;
+        default:
+          throw new Error('Invalid Node Type!');
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    this.transition.stop();
+  }
+
+  onAppear({ yScale, node: { xVal, yVal }, duration }) {
+    this.rect.setAttribute('width', xVal);
+    this.rect.setAttribute('height', yScale.bandwidth());
+    this.text.setAttribute('x', xVal - 3);
+
+    const interp0 = interpolateTransformSvg('translate(0,500)', `translate(0,${yVal})`);
+    const interp1 = interpolateNumber(1e-6, 1);
+
+    this.transition = timer((elapsed) => {
+      const t = elapsed < duration ? (elapsed / duration) : 1;
+      this.node.setAttribute('transform', interp0(t));
+      this.node.setAttribute('opacity', interp1(t));
+      if (t === 1) {
+        this.transition.stop();
+      }
+    });
+  }
+
+  onUpdate({ yScale, node: { xVal, yVal }, duration }, next) {
+    const transformBeg = `translate(0,${yVal})`;
+    const transformEnd = `translate(0,${next.node.yVal})`;
+    const interp0 = interpolateTransformSvg(transformBeg, transformEnd);
+
+    const begVals = { w: xVal, h: yScale.bandwidth(), x: xVal - 3 };
+    const endVals = { w: next.node.xVal, h: next.yScale.bandwidth(), x: next.node.xVal - 3 };
+    const interp1 = interpolateObject(begVals, endVals);
+
+    this.node.setAttribute('opacity', 1);
+
+    this.transition = timer((elapsed) => {
+      const t = elapsed < duration ? (elapsed / duration) : 1;
+      const { w, h, x } = interp1(t);
+
+      this.node.setAttribute('transform', interp0(t));
+      this.rect.setAttribute('width', w);
+      this.rect.setAttribute('height', h);
+      this.text.setAttribute('x', x);
+
+      if (t === 1) {
+        this.transition.stop();
+      }
+    });
+  }
+
+  onRemove({ node: { yVal, udid }, removeNode, duration }) {
+    const interp0 = interpolateTransformSvg(`translate(0,${yVal})`, 'translate(0,500)');
+    const interp1 = interpolateNumber(1, 1e-6);
+
+    this.transition = timer((elapsed) => {
+      const t = elapsed < duration ? (elapsed / duration) : 1;
+
+      this.node.setAttribute('transform', interp0(t));
+      this.node.setAttribute('opacity', interp1(t));
+
+      if (t === 1) {
+        this.transition.stop();
+        removeNode(udid);
+      }
+    });
+  }
+
+  render() {
+    const { xScale, yScale, node: { udid, xVal } } = this.props;
+
+    return (
+      <g ref={(d) => { this.node = d; }}>
+        <rect
+          ref={(d) => { this.rect = d; }}
+          className="bar"
+          opacity={0.5}
+          fill="#0097a7"
+        />
+        <text
+          fontSize="8px"
+          fill="white"
+          dy="0.35em"
+          x={-20}
+          y={yScale.bandwidth() / 2}
+        >{udid}</text>
+        <text
+          ref={(d) => { this.text = d; }}
+          fontSize="8px"
+          textAnchor="end"
+          fill="white"
+          dy="0.35em"
+          y={yScale.bandwidth() / 2}
+        >{percentFormat(xScale.invert(xVal))}</text>
+      </g>
+    );
+  }
+}
+
+Bar.propTypes = {
+  node: PropTypes.shape({
+    udid: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    xVal: PropTypes.number.isRequired,
+    yVal: PropTypes.number.isRequired,
+  }).isRequired,
+  xScale: PropTypes.func.isRequired,
+  yScale: PropTypes.func.isRequired,
+  duration: PropTypes.number.isRequired,
+  removeNode: PropTypes.func.isRequired,
+};
