3be86512938f4326ecdf2dccd8e00db3797aab38
Steve Hall <hall.steven.g@gmail.com>
2017-02-27 18:00:51 -0800

    add jss styles to axis ticks

--- docs/site/src/routes/examples/barCharts/statesByAge/example/components/Tick.js
+++ docs/site/src/routes/examples/barCharts/statesByAge/example/components/Tick.js
@@ -4,11 +4,45 @@ import React, { Component, PropTypes } from 'react';
 import { timer } from 'd3-timer';
 import { format } from 'd3-format';
 import { interpolateNumber, interpolateTransformSvg } from 'd3-interpolate';
+import { createStyleSheet } from 'jss-theme-reactor';
+import customPropTypes from 'material-ui/utils/customPropTypes';
 import { APPEAR, UPDATE, REMOVE, REVIVE } from './withSelection';
 
 const percentFormat = format('.1%');
 
+export const styleSheet = createStyleSheet('Tick', (theme) => {
+  return {
+    line: {
+      strokeWidth: 1,
+      pointerEvents: 'none',
+      opacity: 0.2,
+      stroke: theme.palette.text.primary,
+    },
+    text: {
+      fontSize: 9,
+      fill: theme.palette.text.secondary,
+    },
+  };
+});
+
 export default class Tick extends Component {
+  static propTypes = {
+    node: PropTypes.shape({
+      udid: React.PropTypes.string.isRequired,
+      type: React.PropTypes.string.isRequired,
+      data: React.PropTypes.number.isRequired,
+    }).isRequired,
+    xScale0: PropTypes.func.isRequired,
+    xScale1: PropTypes.func.isRequired,
+    yHeight: PropTypes.number.isRequired,
+    duration: PropTypes.number.isRequired,
+    removeNode: PropTypes.func.isRequired,
+  };
+
+  static contextTypes = {
+    theme: customPropTypes.muiRequired,
+    styleManager: customPropTypes.muiRequired,
+  };
 
   componentDidMount() {
     this.onAppear(this.props);
@@ -107,36 +141,21 @@ export default class Tick extends Component {
 
   render() {
     const { yHeight, node: { data } } = this.props;
+    const classes = this.context.styleManager.render(styleSheet);
 
     return (
       <g ref={(d) => { this.tick = d; }}>
         <line
-          style={{ pointerEvents: 'none' }}
           x1={0} y1={0}
           x2={0} y2={yHeight}
-          opacity={0.2}
-          stroke="#fff"
+          className={classes.line}
         />
         <text
-          fontSize="9px"
-          textAnchor="middle"
-          fill="white"
           x={0} y={-5}
+          textAnchor="middle"
+          className={classes.text}
         >{percentFormat(data)}</text>
       </g>
     );
   }
 }
-
-Tick.propTypes = {
-  node: PropTypes.shape({
-    udid: React.PropTypes.string.isRequired,
-    type: React.PropTypes.string.isRequired,
-    data: React.PropTypes.number.isRequired,
-  }).isRequired,
-  xScale0: PropTypes.func.isRequired,
-  xScale1: PropTypes.func.isRequired,
-  yHeight: PropTypes.number.isRequired,
-  duration: PropTypes.number.isRequired,
-  removeNode: PropTypes.func.isRequired,
-};
