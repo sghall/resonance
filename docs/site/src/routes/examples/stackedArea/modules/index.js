578b6df95903dd41548d6204bffe5ad14e3e4d54
Steve Hall <hall.steven.g@gmail.com>
2017-02-28 21:23:27 -0800

    set up actions, action creators, selector and reducer for stacked area

--- docs/site/src/routes/examples/stackedArea/modules/index.js
+++ docs/site/src/routes/examples/stackedArea/modules/index.js
@@ -1,9 +1,12 @@
 // @flow weak
 import { createSelector } from 'reselect';
-import { scaleLinear, scaleBand } from 'd3-scale';
-import { getSortByKey } from 'docs/site/src/utils/helpers';
+import * as shape from 'd3-shape';
+import { scaleLinear, scaleUtc } from 'd3-scale';
+import { utcParse } from 'd3-time-format';
+import { extent, merge } from 'd3-array';
+import { getPath } from 'docs/site/src/utils/helpers';
 import { VIEW, TRBL, EXAMPLE_STORE_KEY } from './constants';
-import { getInitialValues, getPathsAndScales } from './helpers';
+import { getInitialValues } from './helpers';
 
 const dims = [
   VIEW[0] - TRBL[1] - TRBL[3],  // Usable dimensions width
@@ -13,26 +16,15 @@ const dims = [
 // ********************************************************************
 //  ACTIONS
 // ********************************************************************
-export const STACKED_AREA_REMOVE_NODE = 'STACKED_AREA_REMOVE_NODE';
-export const STACKED_AREA_TOGGLE_NAME = 'STACKED_AREA_TOGGLE_NAME';
-export const STACKED_AREA_UPDATE_PATHS = 'STACKED_AREA_UPDATE_PATHS';
+export const STACKED_AREA_TOGGLE_FILTER = 'STACKED_AREA_TOGGLE_FILTER';
 export const STACKED_AREA_CHANGE_OFFSET = 'STACKED_AREA_CHANGE_OFFSET';
 
 // ********************************************************************
 //  ACTION CREATORS
 // ********************************************************************
-export const removeNode = (udid) => ({
-  type: STACKED_AREA_REMOVE_NODE,
-  udid,
-});
-
 export const toggleName = (index) => ({
-  type: STACKED_AREA_TOGGLE_NAME,
+  type: STACKED_AREA_TOGGLE_FILTER,
   index,
-});
-
-export const updatePaths = () => ({
-  type: STACKED_AREA_UPDATE_PATHS,
 });
 
 export const changeOffset = (name) => ({
@@ -43,119 +35,74 @@ export const changeOffset = (name) => ({
 // ********************************************************************
 //  SELECTOR
 // ********************************************************************
-const getRawData = (state) => state[EXAMPLE_STORE_KEY].rawData;
-const getSortKey = (state) => state[EXAMPLE_STORE_KEY].sortKey;
-const getShowTop = (state) => state[EXAMPLE_STORE_KEY].showTop;
+const getData = (state) => state[EXAMPLE_STORE_KEY].data;
+const getFilter = (state) => state[EXAMPLE_STORE_KEY].filter;
+const getOffset = (state) => state[EXAMPLE_STORE_KEY].offset;
 
 export const makeGetSelectedData = () => {
   return createSelector(
-    [getRawData, getSortKey, getShowTop],
-    (rawData, sortKey, showTop) => {
-      const sort = getSortByKey(sortKey);
-      const data = rawData.sort(sort).slice(0, showTop);
+    [getData, getFilter, getOffset],
+    (data, filter, offset) => {
+      const shown = filter.filter((d) => d.show === true);
+      const dates = data.map((d) => utcParse('%Y-%m-%dT%H:%M:%S.%LZ')(d.date));
 
-      const xExtent = [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER];
-      const yDomain = {};
+      let layoutOffset = shape.stackOffsetNone;
 
-      for (let i = 0; i < data.length; i++) {
-        const d = data[i];
-
-        if (d[sortKey] < xExtent[0]) xExtent[0] = d[sortKey];
-        if (d[sortKey] > xExtent[1]) xExtent[1] = d[sortKey];
-
-        yDomain[d.State] = true;
+      if (offset === 'stream') {
+        layoutOffset = shape.stackOffsetSilhouette;
+      } else if (offset === 'expand') {
+        layoutOffset = shape.stackOffsetExpand;
       }
 
-      const xScale = scaleLinear()
+      const layout = shape.stack()
+        .keys(shown)
+        .value((d, key) => d[key])
+        .offset(layoutOffset)(data);
+
+      const xScale = scaleUtc()
         .range([0, dims[0]])
-        .domain([0, xExtent[1]]);
+        .domain([dates[0], dates[dates.length - 1]]);
 
-      const yScale = scaleBand()
-        .rangeRound([0, dims[1]])
-        .padding(0.1)
-        .domain(Object.keys(yDomain));
+      const yScale = scaleLinear()
+        .range([dims[1], 0])
+        .domain(extent(merge(merge(layout))));
 
-      return {
-        sortKey,
-        data: data.map((d) => ({
-          udid: d.State,
-          xVal: xScale(d[sortKey]),
-          yVal: yScale(d.State),
-        })),
-        xScale,
-        yScale,
-      };
+      const paths = [];
+
+      for (let k = 0; k < shown.length; k++) {
+        paths.push({
+          name: shown[k].name,
+          path: getPath(xScale, yScale, layout[k], dates),
+        });
+      }
+
+      return { filter, offset, paths, xScale, yScale };
     },
   );
 };
 
-function updateNodes(state, names, offset) {
-  const { data, dates, mounted, removed } = state;
-
-  const nodes = {};
-
-  const [paths, x, y] = getPathsAndScales(dims, data, names, dates, offset);
-
-  Object.keys(paths).forEach((path) => {
-    nodes[path] = {
-      udid: path,
-      path: paths[path],
-    };
-
-    if (mounted[path] && !removed[path]) {
-      nodes[path].type = 'UPDATING';
-    } else {
-      nodes[path].type = 'ENTERING';
-    }
-  });
-
-  Object.keys(mounted).forEach((key) => {
-    if (!nodes[key] && !removed[key]) {
-      nodes[key] = {
-        udid: mounted[key].udid,
-        path: mounted[key].path,
-        type: 'EXITING',
-      };
-    }
-  });
-
-  return {
-    mounted: nodes,
-    removed: {},
-    names,
-    dates,
-    offset,
-    xScale: x,
-    yScale: y,
-  };
-}
-
 // ********************************************************************
 //  REDUCER
 // ********************************************************************
-const [data, names, dates] = getInitialValues(150);
-const initialState = { data, names, dates, offset: 'stacked' };
+const { data, filter } = getInitialValues(150);
+const initialState = { data, filter, offset: 'stacked' };
 
-function toggleNode(state, action) {
-  const { index } = action;
+function toggleFilter(state, action) {
+  const item = state.filter[action.index];
 
-  const update = [
-    ...state.names.slice(0, index),
-    { name: state.names[index].name, show: !state.names[index].show },
-    ...state.names.slice(index + 1),
+  return [
+    ...state.filter.slice(0, action.index),
+    { name: item.name, show: !item.show },
+    ...state.filter.slice(action.index + 1),
   ];
-
-  return updateNodes(state, update, state.offset);
 }
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case STACKED_AREA_TOGGLE_NAME:
-      return Object.assign({}, state, toggleNode(state, action));
-    case STACKED_AREA_UPDATE_PATHS:
-      return Object.assign({}, state, updateNodes(state, state.names, state.offset));
+    case STACKED_AREA_TOGGLE_FILTER:
+      return Object.assign({}, state, { filter: toggleFilter(state, action) });
     case STACKED_AREA_CHANGE_OFFSET:
-      return Object.assign({}, state, updateNodes(state, state.names, action.name));
+      return Object.assign({}, state, { offset: action.name });
     default:
       return state;
   }
