396e402c0e43c1ca47aa564c42059d4eadf4071d
Steve Hall <hall.steven.g@gmail.com>
2017-02-28 16:41:18 -0800

    remove ages from constants

--- docs/site/src/routes/examples/stackedArea/modules/constants.js
+++ docs/site/src/routes/examples/stackedArea/modules/constants.js
@@ -4,19 +4,6 @@ export const EXAMPLE_STORE_KEY = 'stacked-area';
 export const VIEW = [500, 400];        // ViewBox: Width, Height
 export const TRBL = [30, 20, 10, 30];  // Margins: Top, Right, Bottom, Left
 
-export const AGES = [
-  'Under 5 Years',
-  '5 to 13 Years',
-  '14 to 17 Years',
-  '18 to 24 Years',
-  '16 Years and Over',
-  '18 Years and Over',
-  '15 to 44 Years',
-  '45 to 64 Years',
-  '65 Years and Over',
-  '85 Years and Over',
-];
-
 export const FRUITS = [{
   name: 'Abaca',
   scientific: 'Musa textilis',
