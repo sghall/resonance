2ca3ae42e5aefb5d82db8df49c6fee99620479f2
Steve Hall <hall.steven.g@gmail.com>
2017-02-28 21:22:32 -0800

    refactor stacked area helpers

--- docs/site/src/routes/examples/stackedArea/modules/helpers.js
+++ docs/site/src/routes/examples/stackedArea/modules/helpers.js
@@ -2,9 +2,7 @@
 
 import moment from 'moment';
 import * as shape from 'd3-shape';
-import { extent, merge, shuffle } from 'd3-array';
-import { scaleUtc, scaleLinear } from 'd3-scale';
-import { utcParse } from 'd3-time-format';
+import { shuffle } from 'd3-array';
 import { genRandomSeries } from 'docs/site/src/utils/helpers';
 import { FRUITS } from './constants';
 
@@ -39,50 +37,15 @@ export function getInitialValues(days) {
     items.push(item);
   }
 
-  return [
-    items,
-    Object.keys(names).sort().map((d) => ({ name: d, show: true })),
-    Object.keys(dates).sort(),
-  ];
+  return {
+    data: items,
+    filter: Object.keys(names).sort().map((d) => ({ name: d, show: true })),
+  };
 }
 
-function getPath(x, y, yVals, dates) {
+export function getPath(x, y, yVals, dates) {
   return shape.area()
     .x((d) => x(d))
     .y0((d, i) => y(yVals[i][0]))
     .y1((d, i) => y(yVals[i][1]))(dates);
-}
-
-export function getPathsAndScales(dims, data, names, dates, offset) {
-  names = names.filter((d) => d.show === true).map((d) => d.name);
-  dates = dates.map((d) => utcParse('%Y-%m-%dT%H:%M:%S.%LZ')(d));
-
-  let layoutOffset = shape.stackOffsetNone;
-
-  if (offset === 'stream') {
-    layoutOffset = shape.stackOffsetSilhouette;
-  } else if (offset === 'expand') {
-    layoutOffset = shape.stackOffsetExpand;
-  }
-
-  const layout = shape.stack()
-    .keys(names)
-    .value((d, key) => d[key])
-    .offset(layoutOffset)(data);
-
-  const x = scaleUtc()
-    .range([0, dims[0]])
-    .domain([dates[0], dates[dates.length - 1]]);
-
-  const y = scaleLinear()
-    .range([dims[1], 0])
-    .domain(extent(merge(merge(layout))));
-
-  const paths = {};
-
-  for (let k = 0; k < names.length; k++) {
-    paths[names[k]] = getPath(x, y, layout[k], dates);
-  }
-
-  return [paths, x, y];
 }
