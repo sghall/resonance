da8b58e894d418ec987659271a5ce4607bb68c24
Steve Hall <hall.steven.g@gmail.com>
2017-03-03 14:26:40 -0800

    remove Chart from export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-export Chart from './Chart';
 export Surface from './Surface';
 export NodeManager from './NodeManager';
 export TickManager from './TickManager';
