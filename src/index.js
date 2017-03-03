ef1377944d476f24d01eb2956d4d91aaa2bfd4e5
Steve Hall <hall.steven.g@gmail.com>
2017-03-03 09:42:41 -0800

    remove old components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-export Line from './Line';
-export Axis from './Axis';
-
 export Chart from './Chart';
-export withManagedData from './withManagedData';
 export NodeManager from './NodeManager';
 export TickManager from './TickManager';
 
