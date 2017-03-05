737b3135193a677dda75ec00952104d0a36ff0f6
Steve Hall <hall.steven.g@gmail.com>
2017-03-04 22:19:28 -0800

    remove old integration tests

--- test/karma.tests.js
+++ test/karma.tests.js
@@ -1,7 +1,4 @@
 // @flow weak
 
-const integrationContext = require.context('./integration', true, /\.(js|jsx)$/);
-integrationContext.keys().forEach(integrationContext);
-
 const unitContext = require.context('../src/', true, /\.spec\.(js|jsx)$/);
 unitContext.keys().forEach(unitContext);
