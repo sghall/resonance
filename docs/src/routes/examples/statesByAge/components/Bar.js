77d1ff1bb14263a0d59f864da124129fcd809002
Steve Hall <hall.steven.g@gmail.com>
2017-03-06 15:34:02 -0800

    use new api on bars

--- docs/src/routes/examples/statesByAge/components/Bar.js
+++ docs/src/routes/examples/statesByAge/components/Bar.js
@@ -1,18 +1,13 @@
 // @flow weak
 
 import React, { PureComponent, PropTypes } from 'react';
-import { timer } from 'd3-timer';
-import {
-  interpolateNumber,
-  interpolateObject,
-  interpolateTransformSvg,
-} from 'd3-interpolate';
-import { format } from 'd3-format';
+import * as d3 from 'd3';
+import { transition } from 'resonance';
 import { createStyleSheet } from 'jss-theme-reactor';
 import customPropTypes from 'material-ui/utils/customPropTypes';
 import { APPEAR, UPDATE, REMOVE, REVIVE } from 'resonance/core/types';
 
-const percentFormat = format('.2%');
+const percentFormat = d3.format('.2%');
 
 const styleSheet = createStyleSheet('Bar', (theme) => {
   return {
@@ -57,20 +52,18 @@ export default class Bar extends PureComponent {
     const { props } = this;
 
     if (prev.node !== props.node) {
-      this.transition.stop();
-
       switch (props.node.type) {
         case APPEAR:
           this.onAppear(props);
           break;
         case UPDATE:
-          this.onUpdate(prev, props);
+          this.onUpdate(props);
           break;
         case REMOVE:
           this.onRemove(props);
           break;
         case REVIVE:
-          this.onUpdate(prev, props);
+          this.onUpdate(props);
           break;
         default:
           break;
@@ -78,75 +71,40 @@ export default class Bar extends PureComponent {
     }
   }
 
-  componentWillUnmount() {
-    this.transition.stop();
-  }
-
-  transition = null; // Last transition run (or running)
   node = null; // Root node ref set in render method
   rect = null; // Rect node ref set in render method
   text = null; // Text node ref set in render method
 
-  onAppear({ yScale, node: { xVal, yVal }, duration }) {
-    this.rect.setAttribute('width', xVal);
-    this.rect.setAttribute('height', yScale.bandwidth());
-    this.text.setAttribute('x', xVal - 3);
-
-    const interp0 = interpolateTransformSvg('translate(0,500)', `translate(0,${yVal})`);
-    const interp1 = interpolateNumber(1e-6, 1);
-
-    this.transition = timer((elapsed) => {
-      const t = elapsed < duration ? (elapsed / duration) : 1;
-      this.node.setAttribute('transform', interp0(t));
-      this.node.setAttribute('opacity', interp1(t));
-      if (t === 1) {
-        this.transition.stop();
-      }
-    });
+  onAppear({ yScale, duration, node: { xVal, yVal } }) {
+    transition.call(this, {
+      node: {
+        opacity: [1e-6, 1],
+        transform: ['translate(0,500)', `translate(0,${yVal})`],
+      },
+      rect: { width: xVal, height: yScale.bandwidth() },
+      text: { x: xVal - 3 },
+    }, { duration });
   }
 
-  onUpdate({ yScale, node: { xVal, yVal }, duration }, next) {
-    const beg = this.node.getAttribute('transform');
-    const end = `translate(0,${next.node.yVal})`;
-    const interp0 = interpolateTransformSvg(beg, end);
-
-    const begVals = { w: xVal, h: yScale.bandwidth(), x: xVal - 3 };
-    const endVals = { w: next.node.xVal, h: next.yScale.bandwidth(), x: next.node.xVal - 3 };
-    const interp1 = interpolateObject(begVals, endVals);
-
-    this.node.setAttribute('opacity', 1);
-
-    this.transition = timer((elapsed) => {
-      const t = elapsed < duration ? (elapsed / duration) : 1;
-      const { w, h, x } = interp1(t);
-
-      this.node.setAttribute('transform', interp0(t));
-      this.rect.setAttribute('width', w);
-      this.rect.setAttribute('height', h);
-      this.text.setAttribute('x', x);
-
-      if (t === 1) {
-        this.transition.stop();
-      }
-    });
+  onUpdate({ yScale, duration, node: { xVal, yVal } }) {
+    transition.call(this, {
+      node: {
+        opacity: [1],
+        transform: [`translate(0,${yVal})`],
+      },
+      rect: { width: [xVal], height: [yScale.bandwidth()] },
+      text: { x: [xVal - 3] },
+    }, { duration });
   }
 
-  onRemove({ node, removeNode, duration }) {
-    const beg = this.node.getAttribute('transform');
-    const interp0 = interpolateTransformSvg(beg, 'translate(0,500)');
-    const interp1 = interpolateNumber(1, 1e-6);
-
-    this.transition = timer((elapsed) => {
-      const t = elapsed < duration ? (elapsed / duration) : 1;
-
-      this.node.setAttribute('transform', interp0(t));
-      this.node.setAttribute('opacity', interp1(t));
-
-      if (t === 1) {
-        this.transition.stop();
-        removeNode(node.udid);
-      }
-    });
+  onRemove({ duration, node: { udid }, removeNode }) {
+    d3.select(this.node)
+      .transition().duration(duration)
+      .attr('opacity', 1e-6)
+      .attr('transform', 'translate(0,500)')
+      .on('end', () => {
+        removeNode(udid);
+      });
   }
 
   render() {
@@ -164,7 +122,7 @@ export default class Bar extends PureComponent {
           x={-20}
           className={classes.text}
           y={yScale.bandwidth() / 2}
-        >{udid.slice(4)}</text>
+        >{udid}</text>
         <text
           ref={(d) => { this.text = d; }}
           textAnchor="end"
