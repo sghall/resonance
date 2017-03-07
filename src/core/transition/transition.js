977d043c969a95a2ca8798a7095e6675f06559e0
Steve Hall <hall.steven.g@gmail.com>
2017-03-07 07:45:37 -0800

    remove d3-ease from src dependencies

--- src/core/transition/transition.js
+++ src/core/transition/transition.js
@@ -1,13 +1,15 @@
 // @flow weak
+
 import { now } from 'd3-timer';
 import tween from './tween';
 import schedule from './schedule';
 
+// from https://github.com/d3/d3-ease/blob/master/src/cubic.js
 function easeCubicInOut(t) {
   return ((t *= 2) <= 1 ? t * t * t : (t -= 2) * t * t + 2) / 2; // eslint-disable-line
 }
 
-const defaultTiming = {
+const defaults = {
   time: null,
   delay: 0,
   duration: 250,
@@ -21,8 +23,6 @@ function newId() {
 }
 
 export default function transition(transitions, opts, events) {
-  const timing = { ...defaultTiming, ...opts, time: now() };
-
   Object.keys(transitions).forEach((ref) => {
     const tweens = [];
 
@@ -41,6 +41,7 @@ export default function transition(transitions, opts, events) {
       }
     });
 
+    const timing = { ...defaults, ...opts, time: now() };
     schedule(this, ref, newId(), timing, tweens, events);
   });
 }
