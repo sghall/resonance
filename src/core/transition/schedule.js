d75c6dc5c20e9bb8ad78cbf6d335e7475f0eb2f0
Steve Hall <hall.steven.g@gmail.com>
2017-03-08 09:52:19 -0800

    update comment

--- src/core/transition/schedule.js
+++ src/core/transition/schedule.js
@@ -1,5 +1,5 @@
 // @flow weak
-// see https://github.com/d3/d3-transition/blob/master/src/transition/schedule.js
+// Apapted from https://github.com/d3/d3-transition/blob/master/src/transition/schedule.js
 
 import { timer, timeout } from 'd3-timer';
 import { CREATED, SCHEDULED, STARTING, STARTED, RUNNING, ENDING, ENDED } from './phases';
