a80ece9f1af9a974e7387e427e35befc99336600
Steve Hall <hall.steven.g@gmail.com>
2017-03-09 10:52:44 -0800

    add react perf

--- docs/src/index.js
+++ docs/src/index.js
@@ -4,6 +4,7 @@ import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
 import React from 'react';
 import { render } from 'react-dom';
+import ReactPerf from 'react-addons-perf';
 import store from 'docs/src/store';
 import App from 'docs/src/components/App';
 
@@ -13,6 +14,8 @@ import App from 'docs/src/components/App';
 // if (process.env.NODE_ENV !== 'production') {
 //   a11y(React, { includeSrcNode: true, ReactDOM });
 // }
+
+window.Perf = ReactPerf;
 
 const rootEl = document.querySelector('#app');
 
