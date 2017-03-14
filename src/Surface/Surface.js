6ed25e911c65b2eb61d8205dec15ea31c043f7a2
Steve Hall <hall.steven.g@gmail.com>
2017-03-13 18:48:33 -0700

    use css bottom padding hack for better cross browser support

--- src/Surface/Surface.js
+++ src/Surface/Surface.js
@@ -13,19 +13,19 @@ import React, { PropTypes } from 'react';
  */
 export default function Surface(props) {
   const { className, view, trbl, children, ...other } = props;
+  const paddingBottom = `${Math.round((view[1] / view[0]) * 100)}%`;
 
+  // uses bottom-padding hack. See https://css-tricks.com/scale-svg/
   return (
     <div
       className={className}
-      style={{
-        width: '100%',
-        height: '0px',
-        paddingTop: `${Math.round(view[1] / (view[0] * 100))}%`,
-        position: 'relative',
-      }}
+      style={{ position: 'relative', width: '100%', height: '0px', paddingBottom }}
       {...other}
     >
-      <svg viewBox={`0 0 ${view[0]} ${view[1]}`}>
+      <svg
+        viewBox={`0 0 ${view[0]} ${view[1]}`}
+        style={{ position: 'absolute', width: '100%', height: '100%', left: 0, top: 0 }}
+      >
         <g transform={`translate(${trbl[3]} ,${trbl[0]})`}>
           {children}
         </g>
