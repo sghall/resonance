a8547a0cf8807355583f8ecc20e1825c004ae091
Steve Hall <hall.steven.g@gmail.com>
2017-03-14 15:26:12 -0700

    add color palette

---
+++ docs/src/utils/palette.js
@@ -0,0 +1,11 @@
+// @flow weak
+
+export default {
+  primary1Color: '#02130C',
+  primary2Color: '#3C564B',
+  primary3Color: '#12291F',
+  accent1Color: '#3A4751',
+  accent2Color: '#1E272E',
+  accent3Color: '#121E26',
+  canvasColor: '#f8f8f9',
+};
