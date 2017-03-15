af729adee4a9dc03bd89edc64cad32ef714ede4d
Steve Hall <hall.steven.g@gmail.com>
2017-03-15 15:51:14 -0700

    only export public components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,3 @@
 export Surface from './Surface';
 export NodeGroup from './NodeGroup';
 export TickGroup from './TickGroup';
-
-export withTransitions from './withTransitions';
-
-export {
-  APPEAR,
-  UPDATE,
-  REMOVE,
-} from './core/types';
