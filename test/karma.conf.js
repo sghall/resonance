47638178b567bcd7ed51ae2821abba175c1b3b57
Steve Hall <hall.steven.g@gmail.com>
2017-03-17 19:44:21 -0700

    update alias

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -71,7 +71,7 @@ module.exports = function(config) {
       },
       resolve: {
         alias: {
-          'material-ui': path.resolve(__dirname, '../src'),
+          resonance: path.resolve(__dirname, '../src'),
           sinon: 'sinon/pkg/sinon.js',
         },
         extensions: ['', '.js', '.jsx', '.json'],
