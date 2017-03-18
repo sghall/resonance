6bdc456cb0df3625cc9af408f4284e2da2bfb9d3
Steve Hall <hall.steven.g@gmail.com>
2017-03-17 17:03:51 -0700

    remove comments

--- docs/webpack-production.config.js
+++ docs/webpack-production.config.js
@@ -4,30 +4,24 @@ const buildPath = path.resolve(__dirname, 'build');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const config = {
-  // Entry point to the project
   entry: [
     './node_modules/babel-polyfill/lib/index.js',
-    path.resolve(__dirname, 'src/app/app.js'),
+    path.resolve(__dirname, 'src/index.js'),
   ],
-  // Webpack config options on how to obtain modules
   resolve: {
-    // When requiring, you don't need to add these extensions
     extensions: ['', '.js', '.md', '.txt'],
     alias: {
-      // material-ui requires will be searched in src folder, not in node_modules
-      'material-ui': path.resolve(__dirname, '../src'),
+      docs: path.resolve(__dirname, '../docs'),
+      resonance: path.resolve(__dirname, '../src'),
     },
   },
-  // Configuration for server
   devServer: {
     contentBase: 'build',
-    // Required for webpack-dev-server
     outputPath: buildPath,
   },
-  // Output file config
   output: {
-    path: buildPath, // Path of output file
-    filename: 'app.js', // Name of output file
+    path: buildPath,
+    filename: 'app.js',
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
@@ -45,19 +39,14 @@ const config = {
         NODE_ENV: JSON.stringify('production'),
       },
     }),
-
-    // Allows error warnings but does not stop compiling. Will remove when eslint is added
     new webpack.NoErrorsPlugin(),
-    // Transfer Files
     new CopyWebpackPlugin([
       {from: 'src/www/css', to: 'css'},
       {from: 'src/www/images', to: 'images'},
       {from: 'src/www/index.html'},
-      {from: 'src/www/versions.json'},
     ]),
   ],
   module: {
-    // Allow loading of non-es5 js files.
     loaders: [
       {
         test: /\.js$/,
