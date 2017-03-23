76f22e4afea59d51520ed11a8db8dd185cddb9ac
Steve Hall <hall.steven.g@gmail.com>
2017-03-23 15:09:04 -0700

    add tests for instance methods

--- src/core/withTransitions/withTransitions.spec.js
+++ src/core/withTransitions/withTransitions.spec.js
@@ -4,11 +4,12 @@
 import React, { Component } from 'react';
 import { assert } from 'chai';
 import sinon from 'sinon';
-import withTransitions from './withTransitions';
 import createMount from 'test/utils/createMount';
+import { APPEAR, UPDATE } from '../types';
+import withTransitions from './withTransitions';
 
 const props = {
-  type: 'APPEAR',
+  type: APPEAR,
   udid: 'unique_id',
   node: {x: 10, y: 20 },
   removeUDID: () => {},
@@ -19,6 +20,32 @@ class Node extends Component {
   line = null // ref set in render
   rect = null // ref set in render
   path = null // ref set in render
+
+  onAppear() {
+    return {
+      line: {
+        x1: [5, 200],
+        y1: [5, 400],
+      },
+      rect: {
+        x: [5, 1000],
+        y: [5, 2000],
+      },
+    };
+  }
+
+  onUpdate() {
+    return {
+      line: {
+        x1: [2000],
+        y1: [4000],
+      },
+      rect: {
+        x: [100],
+        y: [200],
+      },
+    };
+  }
 
   render() {
     return (
@@ -67,5 +94,41 @@ describe('withTransitions', () => {
     const called = NodeWithTransitions.prototype.invokeMethodIfExists.calledOnce;
 
     assert.strictEqual(called, true, 'should have been called once');
+  });
+
+  it('should call the onAppear method of the wrapped instance', () => {
+    const NodeWithTransitions = withTransitions(Node);
+    sinon.spy(Node.prototype, 'onAppear');
+
+    mount(<NodeWithTransitions {...props} />);
+
+    const called = Node.prototype.onAppear.calledOnce;
+
+    assert.strictEqual(called, true, 'should have been called once');
+  });
+
+  it('should NOT call the onUpdate method after mounting', () => {
+    const NodeWithTransitions = withTransitions(Node);
+    const spy = sinon.spy(Node.prototype, 'onUpdate');
+
+    mount(<NodeWithTransitions {...props} />);
+
+    const callCount = Node.prototype.onUpdate.callCount;
+    spy.restore();
+
+    assert.strictEqual(callCount, 0, 'should NOT have been called');
+  });
+
+  it('should call the onUpdate method when type changes to UPDATE', () => {
+    const NodeWithTransitions = withTransitions(Node);
+    const spy = sinon.spy(Node.prototype, 'onUpdate');
+
+    const wrapper = mount(<NodeWithTransitions {...props} />);
+    wrapper.setProps({ ...props, type: UPDATE });
+
+    const callCount = Node.prototype.onUpdate.callCount;
+    spy.restore();
+
+    assert.strictEqual(callCount, 1, 'should have been called once');
   });
 });
