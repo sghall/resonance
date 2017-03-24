c01e9ac1fdbf77a2009dffa21b773b998ce46e25
Steve Hall <hall.steven.g@gmail.com>
2017-03-23 18:59:32 -0700

    clean up TickGroup docs

--- docs/src/routes/documentation/TickGroup/index.js
+++ docs/src/routes/documentation/TickGroup/index.js
@@ -2,48 +2,17 @@
 
 import React, { Component, PropTypes } from 'react';
 import Title from 'react-title-component';
-// import CodeExample from 'docs/src/components/CodeExample';
+import CodeExample from 'docs/src/components/CodeExample';
 import MarkdownElement from 'docs/src/components/MarkdownElement';
 import PropTypeDescription from 'docs/src/components/PropTypeDescription';
 
-// import AppBarExampleIcon from './ExampleIcon';
-// import AppBarExampleIconButton from './ExampleIconButton';
-// import AppBarExampleComposition from './ExampleComposition';
+import Example1 from './Example1';
+import Example2 from './Example2';
 
-class SurfaceDocs extends Component {
+class TickGroupDocs extends Component {
   static propTypes = {
     route: PropTypes.object.isRequired,
   }
-
-  // render() {
-  //   const { route: { docContext, srcContext } } = this.props;
-
-  //   return (
-  //     <div>
-  //       <Title render={(previousTitle) => `TickGroup - ${previousTitle}`} />
-  //       <MarkdownElement text={docContext('./TickGroup/README.md')} />
-  //       <CodeExample
-  //         code={docContext('./TickGroup/ExampleIcon')}
-  //         title="Simple example"
-  //       >
-  //         <AppBarExampleIcon />
-  //       </CodeExample>
-  //       <CodeExample
-  //         code={docContext('./TickGroup/ExampleIconButton')}
-  //         title="Buttons example"
-  //       >
-  //         <AppBarExampleIconButton />
-  //       </CodeExample>
-  //       <CodeExample
-  //         code={docContext('./TickGroup/ExampleComposition')}
-  //         title="Composition example"
-  //       >
-  //         <AppBarExampleComposition />
-  //       </CodeExample>
-  //       <PropTypeDescription code={srcContext('./TickGroup/TickGroup')} />
-  //     </div>
-  //   );
-  // }
 
   render() {
     const { route: { docContext, srcContext } } = this.props;
@@ -52,10 +21,22 @@ class SurfaceDocs extends Component {
       <div>
         <Title render={(previousTitle) => `TickGroup - ${previousTitle}`} />
         <MarkdownElement text={docContext('./TickGroup/README.md')} />
+        <CodeExample
+          code={docContext('./Surface/Example1')}
+          title="Example 1: Basic Surface"
+        >
+          <Example1 />
+        </CodeExample>
+        <CodeExample
+          code={docContext('./Surface/Example2')}
+          title="Example 2: Multiple Surfaces"
+        >
+          <Example2 />
+        </CodeExample>
         <PropTypeDescription code={srcContext('./TickGroup/TickGroup')} />
       </div>
     );
   }
 }
 
-export default SurfaceDocs;
+export default TickGroupDocs;
