72adf5d212fdd4fd72634f82d33de9af6ee4625a
Steve Hall <hall.steven.g@gmail.com>
2017-03-26 14:44:13 -0700

    update TickGroup docs

--- docs/src/routes/documentation/TickGroup/index.js
+++ docs/src/routes/documentation/TickGroup/index.js
@@ -2,12 +2,12 @@
 
 import React, { PropTypes } from 'react';
 import Title from 'react-title-component';
-import CodeExample from 'docs/src/components/CodeExample';
+// import CodeExample from 'docs/src/components/CodeExample';
 import MarkdownElement from 'docs/src/components/MarkdownElement';
 import PropTypeDescription from 'docs/src/components/PropTypeDescription';
 
-import Example1 from './Example1';
-import Example2 from './Example2';
+// import Example1 from './Example1';
+// import Example2 from './Example2';
 
 const TickGroupDocs = (props) => {
   const { route: { docContext, srcContext } } = props;
@@ -16,18 +16,6 @@ const TickGroupDocs = (props) => {
     <div>
       <Title render={(previousTitle) => `TickGroup - ${previousTitle}`} />
       <MarkdownElement text={docContext('./TickGroup/README.md')} />
-      <CodeExample
-        code={docContext('./TickGroup/Example1')}
-        title="Example 1: Basic Surface"
-      >
-        <Example1 />
-      </CodeExample>
-      <CodeExample
-        code={docContext('./TickGroup/Example2')}
-        title="Example 2: Multiple Surfaces"
-      >
-        <Example2 />
-      </CodeExample>
       <PropTypeDescription code={srcContext('./TickGroup/TickGroup')} />
     </div>
   );
