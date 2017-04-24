4a9880670b0cf1e71a75e3ae57bd5fb90c388e37
Steve Hall <hall.steven.g@gmail.com>
2017-04-23 21:44:12 -0700

    remove old exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-export Surface from './Surface';
-
-export NodeGroup from './NodeGroup';
-export TickGroup from './TickGroup';
-
 export createNodeGroup from './createNodeGroup';
 export createTickGroup from './createTickGroup';
