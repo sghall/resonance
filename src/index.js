a563b7156aa8b1d7b2baadd598b9ac4ae65afce7
Steve Hall <hall.steven.g@gmail.com>
2017-04-28 16:19:22 -0700

    add export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
+export Transition from './Transition';
+
 export createNodeGroup from './createNodeGroup';
 export createTickGroup from './createTickGroup';
