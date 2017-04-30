fb78ab17e187887b299c02f74dc0e6661c86ff26
Steve Hall <hall.steven.g@gmail.com>
2017-04-29 22:53:39 -0700

    sketch out node props

--- src/Node/Node.js
+++ src/Node/Node.js
@@ -3,7 +3,6 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import transition from './transition';
 import stop from './stop';
-import { getDisplayName } from '../helpers';
 import { ENTER, UPDATE, EXIT } from '../types';
 
 export const propTypes = {
@@ -14,96 +13,95 @@ export const propTypes = {
     PropTypes.array, // NodeGroup data
     PropTypes.func,  // TickGroup scale
   ]),
+  getInitialState: PropTypes.func.isRequired,
+  onEnter: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
+  onExit: PropTypes.func.isRequired,
   removeUDID: PropTypes.func.isRequired,
   lazyRemoveUDID: PropTypes.func.isRequired,
 };
 
-function withTransitions(Component) {
-  class TransitionComponent extends PureComponent {
-    static propTypes = propTypes;
+export default class Node extends PureComponent {
+  static propTypes = propTypes;
 
-    static displayName = `withTransitions(${getDisplayName(Component)})`
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-      super(props);
+    (this:any).getNodeRef = this.getNodeRef.bind(this);
+    (this:any).remove = this.remove.bind(this);
+    (this:any).lazyRemove = this.lazyRemove.bind(this);
+  }
 
-      (this:any).getNodeRef = this.getNodeRef.bind(this);
-      (this:any).remove = this.remove.bind(this);
-      (this:any).lazyRemove = this.lazyRemove.bind(this);
-    }
+  state = this.props.getInitialState(this.props.node);
 
-    componentDidMount() {
-      this.invokeMethodIfExists('onEnter');
-    }
+  componentDidMount() {
+    const { node, onEnter } = this.props;
+    transition.call(this, onEnter(node));
+  }
 
-    componentDidUpdate(prev) {
-      const { props } = this;
+  componentWillReceiveProps(next) {
+    const { props } = this;
 
-      if (prev.data !== props.data) {
-        switch (props.type) {
-          case ENTER:
-            this.invokeMethodIfExists('onEnter');
-            break;
-          case UPDATE:
-            this.invokeMethodIfExists('onUpdate');
-            break;
-          case EXIT:
-            this.invokeMethodIfExists('onExit');
-            break;
-          default:
-            break;
-        }
+    if (next.data !== props.data) {
+      switch (next.type) {
+        case ENTER:
+          transition.call(this, next.onEnter(next.node, this.remove));
+          break;
+        case UPDATE:
+          transition.call(this, next.onUpdate(next.node, this.remove));
+          break;
+        case EXIT:
+          transition.call(this, next.onExit(next.node, this.remove));
+          break;
+        default:
+          break;
       }
-    }
-
-    componentWillUnmount() {
-      stop.call(this.node);
-    }
-
-    node = null; // ref for wrapped component
-
-    invokeMethodIfExists(method) {
-      const { node } = this;
-      if (node && node[method]) {
-        transition.call(node, node[method]());
-      }
-    }
-
-    remove() {
-      const { removeUDID, udid } = this.props;
-      removeUDID(udid);
-    }
-
-    lazyRemove() {
-      const { lazyRemoveUDID, udid } = this.props;
-      lazyRemoveUDID(udid);
-    }
-
-    getNodeRef(d) {
-      this.node = d;
-    }
-
-    render() {
-      const props = Object.assign({}, this.props);
-
-      Object.keys(propTypes).forEach((p) => {
-        delete props[p];
-      });
-
-      return (
-        <Component
-          ref={this.getNodeRef}
-          type={this.props.type}
-          data={this.props.node}
-          remove={this.remove}
-          lazyRemove={this.lazyRemove}
-          {...props}
-        />
-      );
     }
   }
 
-  return TransitionComponent;
-}
+  componentWillUnmount() {
+    stop.call(this);
+  }
 
-export default withTransitions;
+  node = null; // ref for wrapped component
+
+  // invokeMethodIfExists(method) {
+  //   const { node } = this;
+  //   if (node && node[method]) {
+  //     transition.call(node, node[method]());
+  //   }
+  // }
+
+  remove() {
+    const { removeUDID, udid } = this.props;
+    removeUDID(udid);
+  }
+
+  lazyRemove() {
+    const { lazyRemoveUDID, udid } = this.props;
+    lazyRemoveUDID(udid);
+  }
+
+  getNodeRef(d) {
+    this.node = d;
+  }
+
+  render() {
+    const props = Object.assign({}, this.props);
+
+    Object.keys(propTypes).forEach((p) => {
+      delete props[p];
+    });
+
+    return (
+      <Component
+        ref={this.getNodeRef}
+        type={this.props.type}
+        data={this.props.node}
+        remove={this.remove}
+        lazyRemove={this.lazyRemove}
+        {...props}
+      />
+    );
+  }
+}
