ecb00e265cd3ebbbcf9668a4eda904b12ce346e5
Steve Hall <hall.steven.g@gmail.com>
2017-04-30 13:42:26 -0700

    remove old code

--- docs/src/routes/documentation/createNodeGroup/Example3.js
+++ docs/src/routes/documentation/createNodeGroup/Example3.js
@@ -2,7 +2,6 @@
 /* eslint react/no-multi-comp: "off" */
 
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
 import NodeGroup from 'resonance/NodeGroup';
 import Surface from 'docs/src/components/Surface';
 import { scaleBand, scaleLinear } from 'd3-scale';
@@ -86,103 +85,6 @@ const data = [
     value: 19,
   },
 ];
-
-// **************************************************
-//  Bar Component
-// **************************************************
-class Bar extends PureComponent {
-  static propTypes = {
-    data: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      value: PropTypes.number.isRequired,
-    }).isRequired,
-    index: PropTypes.number.isRequired,
-    xScale: PropTypes.func.isRequired,
-    yScale: PropTypes.func.isRequired,
-    remove: PropTypes.func.isRequired,
-  }
-
-  state = {
-    opacity: 1e-6,
-    x: 0,
-    fill: 'green',
-    width: this.props.xScale.bandwidth(),
-    height: 0,
-  }
-
-  onEnter = () => ([  // An array!!
-    {
-      opacity: [0.6],
-      width: [this.props.xScale.bandwidth()],
-      height: [this.props.yScale(this.props.data.value)],
-      timing: { duration: 1000 },
-    },
-    {
-      x: [this.props.xScale(this.props.data.name)],
-      timing: { duration: 100 * this.props.index, ease: easePoly },
-    },
-  ])
-
-  onUpdate = () => ([  // An array!!
-    {
-      opacity: [0.6],
-      fill: ['blue', 'grey'],
-      timing: { duration: 2000 },
-    },
-    {
-      x: [this.props.xScale(this.props.data.name)],
-      timing: { duration: 2000, ease: easeExpInOut },
-    },
-    {
-      width: [this.props.xScale.bandwidth()],
-      timing: { duration: 500 },
-    },
-    {
-      height: [this.props.yScale(this.props.data.value)],
-      timing: { delay: 2000, duration: 500 },
-      events: { // Events!!
-        end: () => {
-          this.setState({ fill: 'steelblue' });
-        },
-      },
-    },
-  ])
-
-  onExit = () => ({
-    opacity: [1e-6],
-    fill: 'red',
-    timing: { duration: 1000 },
-    events: { end: this.props.remove },
-  })
-
-  render() {
-    const { x, height, ...rest } = this.state;
-
-    return (
-      <g transform={`translate(${x},0)`}>
-        <rect
-          y={height}
-          height={dims[1] - height}
-          {...rest}
-        />
-        <text
-          x="0"
-          y="20"
-          fill="grey"
-          transform="rotate(90 5,20)"
-        >{`x: ${x}`}</text>
-        <text
-          x="0"
-          y="5"
-          fill="grey"
-          transform="rotate(90 5,20)"
-        >{`name: ${this.props.data.name}`}</text>
-      </g>
-    );
-  }
-}
-
-// const BarGroup = createNodeGroup(Bar, 'g', (d) => d.name);
 
 // **************************************************
 //  Example
