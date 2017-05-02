a235aaf9438ae927b1a0954e122cf47e0f70402e
Steve Hall <hall.steven.g@gmail.com>
2017-05-01 17:03:30 -0700

    add exports

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@
 
 export Transition from './Transition';
 
+export NodeGroup from './NodeGroup';
+export TickGroup from './TickGroup';
+
 export createNodeGroup from './createNodeGroup';
 export createTickGroup from './createTickGroup';
