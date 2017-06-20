cde450176d8f7ba264684e1e535ca46e07f8e9df
Steve Hall <hall.steven.g@gmail.com>
2017-06-20 08:59:10 -0700

    updatee bar chart example

--- docs/src/routes/examples/statesByAge/components/BarChart.js
+++ docs/src/routes/examples/statesByAge/components/BarChart.js
@@ -1,6 +1,6 @@
 // @flow weak
 
-import React from 'react';
+import React, { PureComponent } from 'react';
 import TickGroup from 'resonance/TickGroup';
 import NodeGroup from 'resonance/NodeGroup';
 import Surface from 'docs/src/components/Surface';
@@ -12,236 +12,170 @@ import { VIEW, TRBL, DIMS } from '../module/constants';
 
 const percentFormat = format('.1%');
 
-const BarChart = (props) => {
-  const { data, xScale, yScale, duration } = props;
+class BarChart extends PureComponent {
+  static propTypes = {
+    data: PropTypes.array.isRequired,
+    xScale: PropTypes.func.isRequired,
+    yScale: PropTypes.func.isRequired,
+    duration: PropTypes.number.isRequired,
+  }
 
-  return (
-    <Surface view={VIEW} trbl={TRBL}>
-      <NodeGroup
-        data={data}
-        keyAccessor={(d) => d.name}
+  state = {
+    xScale: this.props.xScale,
+  }
 
-        start={(node) => ({
-          node: {
-            opacity: 1e-6,
-            transform: 'translate(0,500)',
-          },
-          rect: {
-            width: node.xVal,
-            height: yScale.bandwidth(),
-          },
-          text: {
-            x: node.xVal - 3,
-          },
-        })}
+  componentWillReceiveProps() {
+    this.setState(() => ({
+      xScale: this.props.xScale,
+    }));
+  }
 
-        enter={(node) => ({
-          node: {
-            opacity: [1e-6, 1],
-            transform: ['translate(0,500)', `translate(0,${node.yVal})`],
-          },
-          rect: { width: node.xVal, height: yScale.bandwidth() },
-          text: { x: node.xVal - 3 },
-          timing: { duration, ease: easePoly },
-        })}
+  render() {
+    const { data, xScale, yScale, duration } = this.props;
 
-        update={(node) => ({
-          node: {
+    return (
+      <Surface view={VIEW} trbl={TRBL}>
+        <TickGroup
+          scale={xScale}
+
+          start={({ val }) => {
+            console.log(xScale.domain(), this.state.xScale.domain());
+
+            return {
+              opacity: 1e-6,
+              transform: `translate(${this.state.xScale(val)},0)`,
+            };
+          }}
+
+          enter={({ val }) => ({
             opacity: [1],
-            transform: [`translate(0,${node.yVal})`],
-          },
-          rect: { width: [node.xVal], height: [yScale.bandwidth()] },
-          text: { x: [node.xVal - 3] },
-          timing: { duration, ease: easePoly },
-        })}
+            transform: [`translate(${xScale(val)},0)`],
+            timing: { duration, ease: easeExp },
+          })}
 
-        leave={() => ({
-          node: {
+          update={({ val }) => ({
+            opacity: [1],
+            transform: [`translate(${xScale(val)},0)`],
+            timing: { duration, ease: easeExp },
+          })}
+
+          leave={({ val }) => ({
             opacity: [1e-6],
-            transform: ['translate(0,500)'],
-          },
-          timing: { duration, ease: easePoly },
-        })}
-      >
-        {(nodes) => {
-          return (
-            <g>
-              {nodes.map(({ data: { name, xVal }, state }) => {
-                return (
-                  <g {...state.node}>
-                    <rect
-                      fill={palette.primary1Color}
-                      opacity={0.4}
-                      {...state.rect}
-                    />
-                    <text
-                      dy="0.35em"
-                      x={-15}
-                      textAnchor="middle"
-                      fill={palette.textColor}
-                      fontSize={10}
-                      y={yScale.bandwidth() / 2}
-                    >{name}</text>
-                    <text
-                      textAnchor="end"
-                      dy="0.35em"
-                      fill="white"
-                      fontSize={10}
-                      y={yScale.bandwidth() / 2}
-                      {...state.text}
-                    >{percentFormat(xScale.invert(xVal))}</text>
-                  </g>
-                );
-              })}
-            </g>
-          );
-        }}
-      </NodeGroup>
-    </Surface>
-  );
-};
+            transform: [`translate(${xScale(val)},0)`],
+            timing: { duration, ease: easeExp },
+          })}
+        >
+          {(nodes) => {
+            return (
+              <g>
+                {nodes.map(({ key, data: { val }, state }) => {
+                  return (
+                    <g key={key} {...state}>
+                      <line
+                        x1={0}
+                        y1={0}
+                        x2={0}
+                        y2={DIMS[1]}
+                        stroke={palette.textColor}
+                        opacity={0.2}
+                      />
+                      <text
+                        x={0}
+                        y={-5}
+                        textAnchor="middle"
+                        fill={palette.textColor}
+                        fontSize="10px"
+                      >{percentFormat(val)}</text>
+                    </g>
+                  );
+                })}
+              </g>
+            );
+          }}
+        </TickGroup>
+        <NodeGroup
+          data={data}
+          keyAccessor={(d) => d.name}
 
-// const BarChart = (props) => {
-//   const { data, xScale, yScale, duration } = props;
+          start={(node) => ({
+            node: {
+              opacity: 1e-6,
+              transform: 'translate(0,500)',
+            },
+            rect: {
+              width: node.xVal,
+              height: yScale.bandwidth(),
+            },
+            text: {
+              x: node.xVal - 3,
+            },
+          })}
 
-//   return (
-//     <Surface view={VIEW} trbl={TRBL}>
-//       <TickGroup
-//         scale={xScale}
+          enter={(node) => ({
+            node: {
+              opacity: [1e-6, 1],
+              transform: ['translate(0,500)', `translate(0,${node.yVal})`],
+            },
+            rect: { width: node.xVal, height: yScale.bandwidth() },
+            text: { x: node.xVal - 3 },
+            timing: { duration, ease: easePoly },
+          })}
 
-//         start={(tick, index, prevScale) => ({
-//           opacity: 1e-6,
-//           transform: `translate(${prevScale(tick.val)},0)`,
-//         })}
+          update={(node) => ({
+            node: {
+              opacity: [1],
+              transform: [`translate(0,${node.yVal})`],
+            },
+            rect: { width: [node.xVal], height: [yScale.bandwidth()] },
+            text: { x: [node.xVal - 3] },
+            timing: { duration, ease: easePoly },
+          })}
 
-//         enter={(tick, index, prevScale) => ({
-//           opacity: [1e-6, 1],
-//           transform: [
-//             `translate(${prevScale(tick.val)},0)`,
-//             `translate(${xScale(tick.val)},0)`,
-//           ],
-//           timing: { duration, ease: easeExp },
-//         })}
-
-//         update={(tick) => ({
-//           opacity: [1],
-//           transform: [`translate(${xScale(tick.val)},0)`],
-//           timing: { duration, ease: easeExp },
-//         })}
-
-//         leave={(tick, index, prevScale, remove) => ({
-//           opacity: [1e-6],
-//           transform: [`translate(${xScale(tick.val)},0)`],
-//           timing: { duration, ease: easeExp },
-//           events: { end: remove },
-//         })}
-
-//         render={(tick, state) => {
-//           return (
-//             <g {...state}>
-//               <line
-//                 x1={0}
-//                 y1={0}
-//                 x2={0}
-//                 y2={DIMS[1]}
-//                 stroke={palette.textColor}
-//                 opacity={0.2}
-//               />
-//               <text
-//                 x={0}
-//                 y={-5}
-//                 textAnchor="middle"
-//                 fill={palette.textColor}
-//                 fontSize="10px"
-//               >{percentFormat(tick.val)}</text>
-//             </g>
-//           );
-//         }}
-//       />
-//       <NodeGroup
-//         data={data}
-//         keyAccessor={(d) => d.name}
-
-//         start={(node) => ({
-//           node: {
-//             opacity: 1e-6,
-//             transform: 'translate(0,500)',
-//           },
-//           rect: {
-//             width: node.xVal,
-//             height: yScale.bandwidth(),
-//           },
-//           text: {
-//             x: node.xVal - 3,
-//           },
-//         })}
-
-//         enter={(node) => ({
-//           node: {
-//             opacity: [1e-6, 1],
-//             transform: ['translate(0,500)', `translate(0,${node.yVal})`],
-//           },
-//           rect: { width: node.xVal, height: yScale.bandwidth() },
-//           text: { x: node.xVal - 3 },
-//           timing: { duration, ease: easePoly },
-//         })}
-
-//         update={(node) => ({
-//           node: {
-//             opacity: [1],
-//             transform: [`translate(0,${node.yVal})`],
-//           },
-//           rect: { width: [node.xVal], height: [yScale.bandwidth()] },
-//           text: { x: [node.xVal - 3] },
-//           timing: { duration, ease: easePoly },
-//         })}
-
-//         leave={(node, index, remove) => ({
-//           node: {
-//             opacity: [1e-6],
-//             transform: ['translate(0,500)'],
-//           },
-//           timing: { duration, ease: easePoly },
-//           events: { end: remove.lazy },
-//         })}
-
-//         render={(node, state) => {
-//           return (
-//             <g {...state.node}>
-//               <rect
-//                 fill={palette.primary1Color}
-//                 opacity={0.4}
-//                 {...state.rect}
-//               />
-//               <text
-//                 dy="0.35em"
-//                 x={-15}
-//                 textAnchor="middle"
-//                 fill={palette.textColor}
-//                 fontSize={10}
-//                 y={yScale.bandwidth() / 2}
-//               >{node.name}</text>
-//               <text
-//                 textAnchor="end"
-//                 dy="0.35em"
-//                 fill="white"
-//                 fontSize={10}
-//                 y={yScale.bandwidth() / 2}
-//                 {...state.text}
-//               >{percentFormat(xScale.invert(node.xVal))}</text>
-//             </g>
-//           );
-//         }}
-//       />
-//     </Surface>
-//   );
-// };
-
-BarChart.propTypes = {
-  data: PropTypes.array.isRequired,
-  xScale: PropTypes.func.isRequired,
-  yScale: PropTypes.func.isRequired,
-  duration: PropTypes.number.isRequired,
-};
+          leave={() => ({
+            node: {
+              opacity: [1e-6],
+              transform: ['translate(0,500)'],
+            },
+            timing: { duration, ease: easePoly },
+          })}
+        >
+          {(nodes) => {
+            return (
+              <g>
+                {nodes.map(({ key, data: { name, xVal }, state }) => {
+                  return (
+                    <g key={key} {...state.node}>
+                      <rect
+                        fill={palette.primary1Color}
+                        opacity={0.4}
+                        {...state.rect}
+                      />
+                      <text
+                        dy="0.35em"
+                        x={-15}
+                        textAnchor="middle"
+                        fill={palette.textColor}
+                        fontSize={10}
+                        y={yScale.bandwidth() / 2}
+                      >{name}</text>
+                      <text
+                        textAnchor="end"
+                        dy="0.35em"
+                        fill="white"
+                        fontSize={10}
+                        y={yScale.bandwidth() / 2}
+                        {...state.text}
+                      >{percentFormat(xScale.invert(xVal))}</text>
+                    </g>
+                  );
+                })}
+              </g>
+            );
+          }}
+        </NodeGroup>
+      </Surface>
+    );
+  }
+}
 
 export default BarChart;
