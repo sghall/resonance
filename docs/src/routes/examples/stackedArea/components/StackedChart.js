7d5c0b1dbe0d09aaff77d25f3985e1c210fda9a2
Steve Hall <hall.steven.g@gmail.com>
2017-06-20 17:31:09 -0700

    update stacked chart

--- docs/src/routes/examples/stackedArea/components/StackedChart.js
+++ docs/src/routes/examples/stackedArea/components/StackedChart.js
@@ -1,6 +1,6 @@
 // @flow weak
 
-import React from 'react';
+import React, { PureComponent } from 'react';
 import TickGroup from 'resonance/TickGroup';
 import NodeGroup from 'resonance/NodeGroup';
 import Surface from 'docs/src/components/Surface';
@@ -13,120 +13,134 @@ import XAxis from './XAxis';
 
 const numberFormat = format(',');
 
-const StackedChart = (props) => {
-  const { data, xScale, yScale, duration, activeSeries } = props;
+class StackedChart extends PureComponent {
+  static propTypes = {
+    data: PropTypes.array.isRequired,
+    xScale: PropTypes.func.isRequired,
+    yScale: PropTypes.func.isRequired,
+    duration: PropTypes.number.isRequired,
+    activeSeries: PropTypes.string.isRequired,
+  }
 
-  return (
-    <Surface view={VIEW} trbl={TRBL}>
-      <defs>
-        <pattern id="hatch" patternUnits="userSpaceOnUse" width="4" height="4">
-          <path
-            d="M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2"
-            style={{ stroke: palette.textColor, strokeWidth: 2, opacity: 0.5 }}
-          />
-        </pattern>
-      </defs>
-      <TickGroup
-        scale={yScale}
+  state = {
+    yScale0: this.props.yScale,
+    yScale1: this.props.yScale,
+  }
 
-        start={(tick) => ({
-          opacity: 1e-6,
-          transform: `translate(0,${yScale(tick.val)})`,
-        })}
+  componentWillReceiveProps(next) {
+    this.setState(() => ({
+      yScale0: this.props.yScale,
+      yScale1: next.yScale,
+    }));
+  }
 
-        enter={(tick, index, cached) => ({
-          opacity: [1e-6, 1],
-          transform: [
-            `translate(0,${cached(tick.val)})`,
-            `translate(0,${yScale(tick.val)})`,
-          ],
-          timing: { duration, ease: easePoly },
-        })}
+  render() {
+    const { data, xScale, duration, activeSeries } = this.props;
+    const { yScale0, yScale1 } = this.state;
 
-        update={(tick) => ({
-          opacity: [1],
-          transform: [`translate(0,${yScale(tick.val)})`],
-          timing: { duration, ease: easePoly },
-        })}
-
-        leave={(tick, index, cached, remove) => ({
-          opacity: [1e-6],
-          transform: [`translate(0,${yScale(tick.val)})`],
-          timing: { duration, ease: easePoly },
-          events: { end: remove.lazy },
-        })}
-
-        render={(tick, state) => {
-          return (
-            <g {...state}>
-              <line
-                x1={0}
-                y1={0}
-                x2={DIMS[0]}
-                y2={0}
-                stroke={palette.textColor}
-                opacity={0.2}
-              />
-              <text
-                fontSize={'9px'}
-                textAnchor="end"
-                dy=".35em"
-                fill={palette.textColor}
-                x={-10}
-                y={0}
-              >{numberFormat(tick.val)}</text>
-            </g>
-          );
-        }}
-      />
-      <NodeGroup
-        data={data}
-        keyAccessor={(d) => d.name}
-
-        start={(node) => ({
-          opacity: 1e-6,
-          d: node.path,
-        })}
-
-        enter={(node) => ({
-          opacity: [1e-6, 0.8],
-          d: [node.path],
-          timing: { duration, ease: easePoly },
-        })}
-
-        update={(node) => ({
-          opacity: [0.8],
-          d: [node.path],
-          timing: { duration, ease: easePoly },
-        })}
-
-        leave={(node, index, remove) => ({
-          opacity: [1e-6],
-          d: [node.path],
-          timing: { duration, ease: easePoly },
-          events: { end: remove.lazy },
-        })}
-
-        render={(node, state) => {
-          return (
+    return (
+      <Surface view={VIEW} trbl={TRBL}>
+        <defs>
+          <pattern id="hatch" patternUnits="userSpaceOnUse" width="4" height="4">
             <path
-              {...state}
-              fill={activeSeries === node.name ? 'url(#hatch)' : node.fill}
+              d="M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2"
+              style={{ stroke: palette.textColor, strokeWidth: 2, opacity: 0.5 }}
             />
-          );
-        }}
-      />
-      <XAxis xScale={xScale} yScale={yScale} />
-    </Surface>
-  );
-};
+          </pattern>
+        </defs>
+        <TickGroup
+          scale={yScale1}
 
-StackedChart.propTypes = {
-  data: PropTypes.array.isRequired,
-  xScale: PropTypes.func.isRequired,
-  yScale: PropTypes.func.isRequired,
-  duration: PropTypes.number.isRequired,
-  activeSeries: PropTypes.string.isRequired,
-};
+          start={(tick) => ({
+            opacity: 1e-6,
+            transform: `translate(0,${yScale0(tick.val)})`,
+          })}
+
+          enter={({ val }) => ({
+            opacity: [1],
+            transform: [`translate(0,${yScale1(val)})`],
+            timing: { duration, ease: easePoly },
+          })}
+
+          update={({ val }) => ({
+            opacity: [1],
+            transform: [`translate(0,${yScale1(val)})`],
+            timing: { duration, ease: easePoly },
+          })}
+
+          leave={({ val }) => ({
+            opacity: [1e-6],
+            transform: [`translate(0,${yScale1(val)})`],
+            timing: { duration, ease: easePoly },
+          })}
+        >
+          {(nodes) => (
+            <g>
+              {nodes.map(({ key, data: { val }, state }) => (
+                <g key={key} {...state}>
+                  <line
+                    x1={0}
+                    y1={0}
+                    x2={DIMS[0]}
+                    y2={0}
+                    stroke={palette.textColor}
+                    opacity={0.2}
+                  />
+                  <text
+                    fontSize={'9px'}
+                    textAnchor="end"
+                    dy=".35em"
+                    fill={palette.textColor}
+                    x={-10}
+                    y={0}
+                  >{numberFormat(val)}</text>
+                </g>
+              ))}
+            </g>
+          )}
+        </TickGroup>
+        <NodeGroup
+          data={data}
+          keyAccessor={(d) => d.name}
+
+          start={({ path }) => ({
+            opacity: 1e-6,
+            d: path,
+          })}
+
+          enter={() => ({
+            opacity: [0.8],
+            timing: { duration, ease: easePoly },
+          })}
+
+          update={({ path }) => ({
+            opacity: [0.8],
+            d: [path],
+            timing: { duration, ease: easePoly },
+          })}
+
+          leave={({ path }) => ({
+            opacity: [1e-6],
+            d: [path],
+            timing: { duration, ease: easePoly },
+          })}
+        >
+          {(nodes) => (
+            <g>
+              {nodes.map(({ key, data: { name, fill }, state }) => (
+                <path
+                  key={key}
+                  {...state}
+                  fill={activeSeries === name ? 'url(#hatch)' : fill}
+                />
+              ))}
+            </g>
+          )}
+        </NodeGroup>
+        <XAxis xScale={xScale} yScale={yScale1} />
+      </Surface>
+    );
+  }
+}
 
 export default StackedChart;
