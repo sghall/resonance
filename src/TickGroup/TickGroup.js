e04ef71d3dc1732a91b481a5c4a1e92d44fed55b
Steve Hall <hall.steven.g@gmail.com>
2017-06-24 14:44:44 -0700

    flow type annotations for TickGroup

--- src/TickGroup/TickGroup.js
+++ src/TickGroup/TickGroup.js
@@ -2,42 +2,42 @@
 /* eslint max-len: "off" */
 
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import NodeGroup from '../NodeGroup';
+
+type Props = {
+  /**
+   * A [contunous D3 scale](https://github.com/d3/d3-scale#continuous-scales) (i.e. has a "ticks" function). The scale prop is treated as immutable so the ticks will only update if prev.scale !== next.scale.
+   */
+  scale: (value: number) => number,
+  /**
+   * Approximate number of ticks to be rendered. This is passed directly through to the D3 scale ticks function.
+   */
+  tickCount: number,
+  /**
+   * A function that returns the starting state object. The function is passed the tick (the tick is passed as an object with a "val" key e.g. { val: 10.5 }) and the index.
+   */
+  start: (data: {}, index: number) => {} | Array<{}>,
+  /**
+   * A function that **returns an object or array of objects** describing how the state should transform on enter.  The function is passed the tick (the tick is passed as an object with a "val" key e.g. { val: 10.5 }) and index.
+   */
+  enter?: (data: {}, index: number) => {} | Array<{}>,
+  /**
+   * A function that **returns an object or array of objects** describing how the state should transform on update.  The function is passed the tick (the tick is passed as an object with a "val" key e.g. { val: 10.5 }) and index.
+   */
+  update?: (data: {}, index: number) => {} | Array<{}>,
+  /**
+   * A function that **returns an object or array of objects** describing how the state should transform on leave.  The function is passed the tick (the tick is passed as an object with a "val" key e.g. { val: 10.5 }) and index.
+   */
+  leave?: (data: {}, index: number) => {} | Array<{}>,
+  /**
+   * A function that renders the ticks. It should accept an array of ticks as its only argument.  Each tick is an object with the key, data ({ val: 10.5 }), state and a type of 'ENTER', 'UPDATE' or 'LEAVE'.
+   */
+  children: (nodes: Array<{}>) => {},
+};
 
 const tickKeyAccessor = (d) => `tick-${d.val}`;
 
 class TickGroup extends Component {
-  static propTypes = {
-    /**
-     * A [contunous D3 scale](https://github.com/d3/d3-scale#continuous-scales) (i.e. has a "ticks" function). The scale prop is treated as immutable so the ticks will only update if prev.scale !== next.scale.
-     */
-    scale: PropTypes.func.isRequired,
-    /**
-     * Approximate number of ticks to be rendered. This is passed directly through to the D3 scale ticks function.
-     */
-    tickCount: PropTypes.number,
-    /**
-     * A function that returns the starting state object. The function is passed the tick (the tick is passed as an object with a "val" key e.g. { val: 10.5 }) and the index.
-     */
-    start: PropTypes.func.isRequired,
-    /**
-     * A function that **returns an object or array of objects** describing how the state should transform on enter.  The function is passed the tick (the tick is passed as an object with a "val" key e.g. { val: 10.5 }) and index.
-     */
-    enter: PropTypes.func,
-    /**
-     * A function that **returns an object or array of objects** describing how the state should transform on update.  The function is passed the tick (the tick is passed as an object with a "val" key e.g. { val: 10.5 }) and index.
-     */
-    update: PropTypes.func,
-    /**
-     * A function that **returns an object or array of objects** describing how the state should transform on leave.  The function is passed the tick (the tick is passed as an object with a "val" key e.g. { val: 10.5 }) and index.
-     */
-    leave: PropTypes.func,
-    /**
-     * A function that renders the ticks. It should accept an array of ticks as its only argument.  Each tick is an object with the key, data ({ val: 10.5 }), state and a type of 'ENTER', 'UPDATE' or 'LEAVE'.
-     */
-    children: PropTypes.func.isRequired,
-  };
 
   static defaultProps = {
     enter: () => {},
@@ -66,6 +66,8 @@ class TickGroup extends Component {
   updateTicks(ticks) {
     this.setState(() => ({ ticks }));
   }
+
+  props: Props
 
   render() {
     const { start, enter, update, leave, children } = this.props;
