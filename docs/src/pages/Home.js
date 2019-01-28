214e4bf64a0e4d2ff6d2977d9415a3d261e82be2
Steve Hall <hall.steven.g@gmail.com>
2019-01-28 12:55:18 -0700

    update home page

--- docs/src/pages/Home.js
+++ docs/src/pages/Home.js
@@ -6,7 +6,6 @@ import { withStyles } from '@material-ui/core/styles'
 import Link from 'react-router/lib/Link'
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
-import logo from './logo-resonance.png'
 
 const styles = () => ({
   root: {
@@ -20,14 +19,6 @@ function Home(props) {
 
   return (
     <Grid container alignItems="center" justify="space-around">
-      <Grid item xs={10} className={classes.root}>
-        <img
-          style={{ marginLeft: '10%' }}
-          width="80%"
-          src={logo}
-          alt="resonance"
-        />
-      </Grid>
       <Grid item xs={12} className={classes.root}>
         <Button component={Link} variant="outlined" to="/demos/node-group">
           Demos
