f5998eda72445cbd9c60366fe019e38b572970b9
Steve Hall <hall.steven.g@gmail.com>
2019-01-29 20:17:17 -0700

    update home

--- docs/src/pages/Home.js
+++ docs/src/pages/Home.js
@@ -6,6 +6,7 @@ import { withStyles } from '@material-ui/core/styles'
 import Link from 'react-router/lib/Link'
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 
 const styles = () => ({
   root: {
@@ -19,6 +20,14 @@ function Home(props) {
 
   return (
     <Grid container alignItems="center" justify="space-around">
+      <Grid item xs={12} className={classes.root}>
+        <Typography variant="h3">
+          Resonance
+        </Typography>
+        <Typography variant="body1">
+          React Animation Library
+        </Typography>
+      </Grid>
       <Grid item xs={12} className={classes.root}>
         <Button component={Link} variant="outlined" to="/demos/node-group">
           Demos
