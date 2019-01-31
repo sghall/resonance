b3a443b50699769951a45f28596c24727e312977
Steve Hall <hall.steven.g@gmail.com>
2019-01-30 19:03:39 -0700

    add tests

--- src/Animate/index.spec.js
+++ src/Animate/index.spec.js
@@ -18,4 +18,56 @@ describe('<Animate />', () => {
       done()
     }, 50)
   })
+
+  it('should work with no props', done => {
+    const wrapper = mount(
+      <Animate />
+    )
+
+    setTimeout(() => {
+      assert.strictEqual(wrapper.find('div').length, 1)
+      done()
+    }, 50)
+  })
+
+  it('should run a interpolations on enter with no other props', done => {
+    let last = 0
+
+    const i = t => {
+      last = t
+      return t
+    }
+    
+    mount(
+      <Animate enter={{ t: i }} />
+    )
+
+    setTimeout(() => {
+      assert.strictEqual(last, 1)
+      done()
+    }, 300)
+  })
+
+  it('should run a interpolations on update with no other props', done => {
+    let last = 0
+
+    const i = t => {
+      last = t
+      return t
+    }
+    
+    const wrapper = mount(
+      <Animate update={{ t: i }} />
+    )
+
+    setTimeout(() => {
+      assert.strictEqual(last, 0)
+      wrapper.setProps({ show: true })
+    }, 300)
+
+    setTimeout(() => {
+      assert.strictEqual(last, 1)
+      done()
+    }, 600)
+  })
 })
